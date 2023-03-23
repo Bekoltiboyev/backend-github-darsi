1e0db92cd009cf4c91d78b0fcfadc4d15669d17b
Beknazar <oltiboyevbeknazar088@gmail.com>
2023-03-23 18:32:27 +0500

    list loop

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ def github():
     print("hello github")
 
 github()
+
+
+lst  = [34,34,36,89,53]
+for x in lst:
+    print(x)
+
+
+# git init
+# git add .
+# git commit -m "sjdrs srdrjfbdsrfb"
+# git push origin main
